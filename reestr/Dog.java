114a9688116b9242f6a913651f7a5903ea8e09c5
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:38:58 +0300

    add dog

--- reestr/Dog.java
+++ reestr/Dog.java
@@ -1,5 +1,19 @@
 package reestr;
 
-public class Dog {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Dog extends Pet{
+    private String stock; // порода
     
+    public Dog(String type) throws IOException {
+        super(type);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите название породы: ");
+        this.stock = bufferedReader.readLine();
+    }
+    public String getStock() {
+        return stock;
+    }
 }
