26f68b2e1c754e42bc6c8b7b11ae1bef986bee3d
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:40:21 +0300

    add cat

--- reestr/Cat.java
+++ reestr/Cat.java
@@ -1,5 +1,23 @@
 package reestr;
 
-public class Cat {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cat extends Pet{
+
+    private String color;
+
+    public Cat(String type) throws IOException {
+        super(type);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите окрас: ");
+        this.color = bufferedReader.readLine();
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
