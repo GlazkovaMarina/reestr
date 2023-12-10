16b9c901c411c28433d3eb1edebe788bf0ba85d9
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:49:37 +0300

    add donkey

--- reestr/Donkey.java
+++ reestr/Donkey.java
@@ -1,5 +1,19 @@
 package reestr;
 
-public class Donkey {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Donkey extends PackAnimal{
+    private String stock;
+
+    public Donkey(String type) throws IOException {
+        super(type);
+        System.out.println("Введите род: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        this.stock = bufferedReader.readLine();
+    }
+    public String getStock() {
+        return stock;
+    }
 }
