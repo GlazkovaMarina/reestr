d40e9eab2a494dc06a5ef3bf2fc20e92605b8e11
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 09:55:51 +0300

    realized url

---
+++ reestr/Main.java
@@ -0,0 +1,10 @@
+package reestr;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ reestr/PackAnimal.java
@@ -0,0 +1,5 @@
+package reestr;
+
+public class PackAnimal {
+    
+}
