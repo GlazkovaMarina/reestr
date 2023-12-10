5eff58a60848978d74a131d9b324567d308bfaa9
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 10:57:44 +0300

    add Animal class

--- reestr/Animal.java
+++ reestr/Animal.java
@@ -1,5 +1,65 @@
 package reestr;
 
-public class Animal {
-    
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public  class Animal {
+    private String name;
+    private Date birthday;
+    private ArrayList<String> commands;
+
+    public Animal() {
+        String tempBirthday = null;
+        String tempCommands = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите имя животного: ");
+            name = bufferedReader.readLine();
+            System.out.println("Введите дату рождения животного в формате yyyy-mm-dd (Н-р 2020-12-01): ");
+            tempBirthday = bufferedReader.readLine();
+            System.out.println("Введите команды, которые умеет выполнять животное через запятую и пробел (Н-р Сидеть, Лежать, Гавкать): ");
+            tempCommands = bufferedReader.readLine();  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            this.birthday = format.parse(tempBirthday);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String[] str = tempCommands.split("[,\\s]+");
+        this.commands = new ArrayList<>();
+        for (String command: str) {
+            this.commands.add(command);
+        }
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBirthday() {
+        return birthday.toString();
+    }
+    public ArrayList<String> getCommands() {
+        return commands;
+    }
+    public void newCommand(String command) {
+        this.commands.add(command);
+    }
+    @Override
+    public String toString() {
+        return "name = " + name + ", birthday = " + birthday.toString() + ", commands = " + commands;
+    }
 }
+
