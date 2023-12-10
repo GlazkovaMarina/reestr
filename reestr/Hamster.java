eb391b4ffca6ef11405238feafa9d9be32345d88
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:41:43 +0300

    add hamster

--- reestr/Hamster.java
+++ reestr/Hamster.java
@@ -1,5 +1,19 @@
 package reestr;
 
-public class Hamster {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hamster extends Pet{
+    private String genus; // род
+
+    public Hamster(String type) throws IOException {
+        super(type);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите род: ");
+        this.genus = bufferedReader.readLine();
+    }
+    public String getGenus() {
+        return genus;
+    }
 }
