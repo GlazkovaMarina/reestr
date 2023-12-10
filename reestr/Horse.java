0f14cc2a66f1ef37a1bfbeb54162a0b84653f94d
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:47:11 +0300

    add horse

--- reestr/Horse.java
+++ reestr/Horse.java
@@ -1,5 +1,19 @@
 package reestr;
 
-public class Horse {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Horse extends PackAnimal{
+    private String kind;
+
+    public Horse(String type) throws IOException {
+        super(type);
+        System.out.println("Введите род: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        this.kind = bufferedReader.readLine();
+    }
+    public String getKind() {
+        return kind;
+    }
 }
