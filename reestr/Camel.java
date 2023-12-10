0586f1cae413c48ca740b911aecf236d29e8534c
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:48:15 +0300

    add camel

--- reestr/Camel.java
+++ reestr/Camel.java
@@ -1,5 +1,23 @@
 package reestr;
 
-public class Camel {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Camel extends PackAnimal{
     
+    private Integer numberOfHumps;
+
+    public Camel(String type) throws IOException {
+        super(type);
+        System.out.println("Введите количество горбов: ");
+        try (Scanner sc = new Scanner(System.in)) {
+            this.numberOfHumps = sc.nextInt();
+        }
+    }
+    public Integer getNumberOfHumps() {
+        return numberOfHumps;
+    }
+    public void setNumberOfHumps(Integer numberOfHumps) {
+        this.numberOfHumps = numberOfHumps;
+    }
 }
