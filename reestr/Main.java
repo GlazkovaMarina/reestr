3ca09da7abade40391626048928eafdb7790ef49
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 15:22:10 +0300

    add switch

--- reestr/Main.java
+++ reestr/Main.java
@@ -2,7 +2,6 @@ package reestr;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import java.util.Scanner;
 
 public class Main {
@@ -36,7 +35,7 @@ public class Main {
         int number = -1;
         while (number < 0 || number > 5) {
             System.out.println(
-                    "Введите цифру животного, которое хотели бы добавить (0 - Собака, 1 - Кошка, 2 - Хомяк, 3 - Лошадь, 4 - Верблюд, 5 -Осел): ");
+                    "\nВведите цифру животного, которое хотели бы добавить (0 - Собака, 1 - Кошка, 2 - Хомяк, 3 - Лошадь, 4 - Верблюд, 5 -Осел): ");
             number = sc.nextInt();
             if (number < 0 || number > 5) {
                 System.out.println("ОШИБКА! Некoрректный ввод!");
@@ -46,28 +45,39 @@ public class Main {
         }
     }
 
+    public static void PrintReestr(ArrayList<Object> reestr) throws IOException {
+        int i = 1;
+        System.out.println("\nРеестр:");
+        for (Object animal : reestr) {
+            System.out.printf("%d) ", i++);
+            System.out.println(animal.toString());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         ArrayList reestr = new ArrayList<Object>();
-
         Scanner sc = new Scanner(System.in);
         int number = -1;
         while (number != 1 || number != 2 || number != 0) {
             System.out.println(
-                    "Это реестр животных!\nВыберите действие:\n1 - завести новое животное;\n2 - вывести список животных в реестре;\n0 - завершить работу с реестром!");
+                    "\nОСНОВНОЕ МЕНЮ\nЭто реестр животных!\nВыберите действие:\n1 - завести новое животное;\n2 - вывести список животных в реестре;\n3 - вывести команды, выбранного животного;\n0 - завершить работу с реестром!");
             number = sc.nextInt();
-            if (number == 1) {
-                AddAnimal(reestr);
-            } else if (number == 2) {
-                int i = 1;
-                for (Object animal : reestr) {
-                    System.out.printf("%d) ", i++);
-                    System.out.println(animal.toString());
-                }
-            } else if (number == 0) {
-                System.out.println("Работа с реестром завершена! До свидания!");
-                break;
-            } else {
-                System.out.println("ОШИБКА! Некoрректный ввод!");
+            switch (number) {
+                case 0:
+                    System.out.println("Работа с реестром завершена! До свидания!");
+                    break;
+                case 1:
+                    AddAnimal(reestr);
+                    break;
+                case 2:
+                    PrintReestr(reestr);
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    System.out.println("ОШИБКА! Некoрректный ввод!");
+                    break;
             }
         }
 
