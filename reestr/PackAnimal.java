de2ce410833f5c0ba096bc98385a5e4444530171
GlazkovaMarina <glazkova.mv@bk.ru>
2023-12-10 11:45:51 +0300

    add packanimal

--- reestr/PackAnimal.java
+++ reestr/PackAnimal.java
@@ -1,5 +1,27 @@
 package reestr;
 
-public class PackAnimal {
-    
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PackAnimal  extends Animal{
+    private Double bearingForce;
+    private String type;
+
+    public PackAnimal(String type) throws IOException {
+        super();
+        System.out.println("Введите грузоподъемность (разделитель - запятая): ");
+        try (Scanner sc = new Scanner(System.in)) {
+            this.bearingForce = sc.nextDouble();
+        }
+        this.type = type;
+    }
+    public Double getBearingForce() {
+        return bearingForce;
+    }
+    public void setBearingForce(Double bearingForce) {
+        this.bearingForce = bearingForce;
+    }
+    public String getType() {
+        return type;
+    }
 }
